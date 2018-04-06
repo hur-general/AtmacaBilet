ad13c27ad243e27017777ac1af97a4a8c1d94ed8
hur-general <mdurmus@ogr.iu.edu.tr>
2018-04-06 21:35:19 +0300

    init

---
+++ src/main/java/com/timposu/jsf/model/User.java
@@ -0,0 +1,41 @@
+package com.timposu.jsf.model;
+
+public class User {
+    private String firstName;
+    private String lastname;
+    private String email;
+    private String password;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
